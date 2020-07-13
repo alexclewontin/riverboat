59c6b2ea40b17da9f1ea84c5f19d503ec6788418
Alex Lewontin <alex.c.lewontin@gmail.com>
2020-07-13 12:37:13 -0400

    Add/update licenses for all maintained files

--- errors.go
+++ errors.go
@@ -1,4 +1,4 @@
-//* Copyright (c) 2020, AUTHOR
+//* Copyright (c) 2020, AUTHOR, Alex Lewontin
 //* All rights reserved.
 //* 
 //* Redistribution and use in source and binary forms, with or without
@@ -20,6 +20,7 @@
 //* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 //* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 package riverboat
 
 import (
